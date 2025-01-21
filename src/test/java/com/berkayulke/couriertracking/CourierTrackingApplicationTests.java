ff1feb6c91bb22ab6946c9589be5ce6d6300c08b
T095566 <berkay.ulke@ykteknoloji.com.tr>
2025-01-21 13:03:08 +0300

    initial commit for spring boot + sqlite

---
+++ src/test/java/com/berkayulke/couriertracking/CourierTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berkayulke.couriertracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierTrackingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
