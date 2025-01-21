139cf3a65892a317a68b4eff6a3025ac7453c7aa
T095566 <berkay.ulke@ykteknoloji.com.tr>
2025-01-21 13:27:17 +0300

    add store entity and hibernate configuration

---
+++ src/main/java/com/berkayulke/couriertracking/Store.java
@@ -0,0 +1,27 @@
+package com.berkayulke.couriertracking;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "stores")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Store {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Double lat;
+
+    @Column(nullable = false)
+    private Double lng;
+
+}
